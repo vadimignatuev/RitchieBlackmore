5221097b3b9043351393e326067fa8371d70ad82
vadimignatuev <vadimigna@yandex.ru>
2015-08-07 12:44:36 +0300

    correct work DBcontext but dont  correct scripts

--- RitchieBlackmore/Classes/EntityFramworcDbContex/RitchieBlackmore.Context.cs
+++ RitchieBlackmore/Classes/EntityFramworcDbContex/RitchieBlackmore.Context.cs
@@ -12,8 +12,7 @@ namespace RitchieBlackmore.Classes.EntityFramworcDbContex
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
-    using System.Data.Objects;
-    using System.Data.Objects.DataClasses;
+    using System.Data.Entity.Core.Objects;
     using System.Linq;
     
     public partial class RitchieBlackmoreEntities : DbContext
